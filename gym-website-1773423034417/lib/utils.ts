2a7119de24b6058288f7697310a2e5919f9c3c3f
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 23:00:38 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773423034417/lib/utils.ts
@@ -0,0 +1,7 @@
+export function formatPhoneNumber(phoneNumber: string) {
+  return phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+}
+
+export function formatEmail(email: string) {
+  return email.toLowerCase();
+}
